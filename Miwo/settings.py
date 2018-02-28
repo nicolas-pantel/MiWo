a646f507b3ab1dded89d47b3a4e6cb8c7e7c9ca3
npantel <nicolas.pantel@gmail.com>
2018-02-28 22:17:48 +0100

    feature(code): Django-Heroku module

--- Miwo/settings.py
+++ Miwo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
