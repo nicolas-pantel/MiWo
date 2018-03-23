d7e9a1ae1b3286b010b7ee85412486f775121539
npantel <nicolas.pantel@gmail.com>
2018-03-23 17:05:58 +0100

    feature(api): Dispaly built-in API doc on /docs

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
+from rest_framework.documentation import include_docs_urls
+
 from . import views
 
 
@@ -9,6 +11,7 @@ urlpatterns = [
     path('demo/<channel>', views.DemoServerView.as_view(), name="demo_server"),
 
     ### API ###
+    path('api/docs/', include_docs_urls(title='Miwo API')),
     path('api/', include('rest_auth.urls')),
     path('api/registration/', include('rest_auth.registration.urls')),
 ]
