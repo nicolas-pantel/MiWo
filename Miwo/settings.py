0d9e0dcb982cf7d70059c3e2845142dcb6f2f742
npantel <nicolas.pantel@gmail.com>
2018-04-04 21:38:03 +0200

    feature(code): CORS

--- Miwo/settings.py
+++ Miwo/settings.py
@@ -55,11 +55,13 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'rest_auth.registration',
     'bootstrap4',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -180,3 +182,6 @@ REST_FRAMEWORK = {
 }
 ACCOUNT_LOGOUT_ON_GET = True
 LOGIN_REDIRECT_URL = "/"
+
+# CORS
+CORS_ORIGIN_ALLOW_ALL = True
