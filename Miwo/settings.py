082f177247f72f9ab467c0a2968a62be3c9c80fd
npantel <nicolas.pantel@gmail.com>
2018-04-17 22:38:17 +0200

    fix(bo): Back to gunicorn and wsgi

--- Miwo/settings.py
+++ Miwo/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backoffice',
-    'channels',
+    # 'channels',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: daphne Miwo.asgi:application --port $PORT --bind 0.0.0.0
+web: gunicorn Miwo.wsgi --log-file -
