2e521b7b5a19e825292afc2be3e4a3d3a415d468
npantel <nicolas.pantel@gmail.com>
2018-04-24 15:25:52 +0200

    fix(code): Clean api file

--- backoffice/api_views.py
+++ backoffice/api_views.py
@@ -15,16 +15,6 @@ class InfluencersAPIView(generics.ListAPIView):
     def get_queryset(self):
         """Filter influencers only"""
         return models.MiwoUser.objects.filter(campaigns__isnull=False).distinct()
-'''
-    def get(self, request, *args, **kwargs):
-        """Return the list of influencers"""
-        data = [
-            {"pk": 1, "username": "influencer 1", "candidat_picture": "https://loremflickr.com/150/150/face?lock=1"},
-            {"pk": 2, "username": "influencer 2", "candidat_picture": "https://loremflickr.com/150/150/face?lock=2"},
-            {"pk": 3, "username": "influencer 3", "candidat_picture": "https://loremflickr.com/150/150/face?lock=3"},
-        ]
-        return Response(data)
-'''
 
 
 class InfluencerSubscriptionAPIView(APIView):
@@ -47,16 +37,6 @@ class InfluencersFavoritesAPIView(generics.ListAPIView):
     def get_queryset(self):
         """Filter favorite influencers only"""
         return self.request.user.favorite_influencers.all()
-'''
-    def get(self, request, *args, **kwargs):
-        """Return user's favorite influencers"""
-        data = [
-            {"pk": 1, "username": "influencer 1", "candidat_picture": "https://loremflickr.com/150/150/face?lock=1"},
-            {"pk": 2, "username": "influencer 2", "candidat_picture": "https://loremflickr.com/150/150/face?lock=5"},
-            {"pk": 3, "username": "influencer 3", "candidat_picture": "https://loremflickr.com/150/150/face?lock=6"},
-        ]
-        return Response(data)
-'''
 
 
 class InfluencersSearchAPIView(generics.ListAPIView):
@@ -67,22 +47,6 @@ class InfluencersSearchAPIView(generics.ListAPIView):
     def get_queryset(self):
         """Search for favorite influencers containing search text"""
         return self.request.user.favorite_influencers.filter(username__icontains=self.kwargs["search_text"])
-'''
-    def get(self, request, search_text, *args, **kwargs):
-        """Return list of influenceurs matching search_text"""
-        data = [
-            {
-                "pk": 5, "username": "{} the influ".format(search_text),
-                "candidat_picture": "https://loremflickr.com/150/150/face?lock=1"},
-            {
-                "pk": 8, "username": "super{}".format(search_text),
-                "candidat_picture": "https://loremflickr.com/150/150/face?lock=6"},
-            {
-                "pk": 9, "username": "top_{}_x".format(search_text),
-                "candidat_picture": "https://loremflickr.com/150/150/face?lock=7"},
-        ]
-        return Response(data)
-'''
 
 
 class PublicationsAPIView(generics.ListAPIView):
@@ -94,29 +58,6 @@ class PublicationsAPIView(generics.ListAPIView):
         """List of influencer's publications"""
         return models.Publication.objects.filter(campaign__user__pk=self.kwargs["influencer_pk"])
 
-'''
-    def get(self, request, influencer_pk, *args, **kwargs):
-        """Return the list of an influencer's publications"""
-        data = [
-            {
-                "id_news": 10, "title_news": "First pub of influ {}".format(influencer_pk), "type": "video",
-                "date_news": "April 21, 2018", "picture_news": "https://loremflickr.com/150/150/youtube?lock=1",
-                "nb_products": "2 products", "timeline_value": 25, "social_network": "youtube",
-            },
-            {
-                "id_news": 11, "title_news": "Second pub of influ {}".format(influencer_pk), "type": "video",
-                "date_news": "April 23, 2018", "picture_news": "https://loremflickr.com/150/150/youtube?lock=2",
-                "nb_products": "5 products", "timeline_value": 12, "social_network": "youtube",
-            },
-            {
-                "id_news": 13, "title_news": "Third pub of influ {}".format(influencer_pk), "type": "video",
-                "date_news": "April 24, 2018", "picture_news": "https://loremflickr.com/150/150/youtube?lock=3",
-                "nb_products": "1 products", "timeline_value": 2, "social_network": "youtube",
-            },
-        ]
-        return Response(data)
-'''
-
 
 class TagsAPIView(generics.ListAPIView):
     """Return the list of tags linked to a publication"""
@@ -126,31 +67,6 @@ class TagsAPIView(generics.ListAPIView):
     def get_queryset(self):
         """List of tags for a publication"""
         return models.TagVideo.objects.filter(publication__pk=self.kwargs["publication_pk"])
-'''
-    def get(self, request, publication_pk, *args, **kwargs):
-        """Return the list of tags linked to a publication"""
-        data = [
-            {
-                # "pk": 20, "title": "First tag of publi {}".format(publication_pk),
-                "id_product": 20, "nom_product": "Shoe",
-                "desc_product": "White striped shoes. Sportswear.",
-                "prix_produit": 120,
-                "pics_produit": "https://loremflickr.com/150/150/shoe?lock=3"},
-            {
-                # "pk": 21, "title": "Second tag of publi {}".format(publication_pk),
-                "id_product": 205, "nom_product": "Hat",
-                "desc_product": "Red hat.",
-                "prix_produit": 35,
-                "pics_produit": "https://loremflickr.com/150/150/cat?lock=6"},
-            {
-                # "pk": 22, "title": "Third tag of publi {}".format(publication_pk),
-                "id_product": 207, "nom_product": "Jacket",
-                "desc_product": "Blue jacket. Sportswear.",
-                "prix_produit": 120,
-                "pics_produit": "https://loremflickr.com/150/150/cat?lock=1"},
-        ]
-        return Response(data)
-'''
 
 
 class TagAPIView(generics.RetrieveAPIView):
@@ -158,24 +74,6 @@ class TagAPIView(generics.RetrieveAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = serializers.TagSerializer
     queryset = models.TagVideo.objects.all()
-'''
-    def get(self, request, publication_pk, tag_pk, *args, **kwargs):
-        """Return details of a tag linked to a publication"""
-        data = {
-            "pk": 20, "title": "First tag of publi {}".format(publication_pk),
-            "id_product": 20,
-            "nom_product": "Shoe", "product_price": 70,
-            "desc_product": "White striped shoes. Sportswear.",
-            "prix_produit": 120,
-            "pics_produit": [
-                'https://loremflickr.com/500/500/shoe?lock=3',
-                'https://loremflickr.com/500/500/shoes?lock=4',
-                'https://loremflickr.com/500/500/shoes?lock=5',
-            ],
-            "purchase_link": "http://vendor.com/products/shoe/",
-        }
-        return Response(data)
-'''
 
 
 class TagVideoFavoritesListView(TagsAPIView):
