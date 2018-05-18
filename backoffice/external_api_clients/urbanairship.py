f1ade0b83edf87218c3d1c9a8857441ec3e83af6
npantel <nicolas.pantel@gmail.com>
2018-05-18 17:37:24 +0200

    fix(bo): Do not recycle urbanairship pushs

--- backoffice/external_api_clients/urbanairship.py
+++ backoffice/external_api_clients/urbanairship.py
@@ -7,17 +7,17 @@ from backoffice import models
 
 def publish(user, publication):
     """Send notifaction to followers on publication"""
-    airship = ua.Airship(settings.URBANAIRSHIP_KEY, settings.URBANAIRSHIP_MASTER_SECRET)
-    push = airship.create_push()
-    push.notification = ua.notification(
-        ios=ua.ios(
-            alert="New publication from {}".format(user.username),
-            badge='+1',
-        )
-    )
-    push.device_types = ua.device_types('ios')
     followers = user.followers.all()
     for follower in followers:
+        airship = ua.Airship(settings.URBANAIRSHIP_KEY, settings.URBANAIRSHIP_MASTER_SECRET)
+        push = airship.create_push()
+        push.notification = ua.notification(
+            ios=ua.ios(
+                alert="New publication from {}".format(user.username),
+                badge='+1',
+            )
+        )
+        push.device_types = ua.device_types('ios')
         devices = models.Device.objects.filter(profile=follower.profile)
         if devices.exists():
             device = devices[0]
