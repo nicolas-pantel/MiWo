4c7efe384c4851a700b0ad7135bcae8183da1937
npantel <nicolas.pantel@gmail.com>
2018-05-28 18:23:35 +0200

    fix(api): Translate date and products

--- backoffice/serializers.py
+++ backoffice/serializers.py
@@ -2,7 +2,7 @@ from rest_auth import serializers as rest_auth_serializers
 from rest_auth.registration import serializers as rest_auth_registration_serializers
 from rest_framework import serializers
 
-from django.utils import timezone
+from django.utils import timezone, formats
 
 from . import models
 
@@ -78,13 +78,13 @@ class PublicationsSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         """Convert some fields to client format."""
         ret = super().to_representation(instance)
-        ret['date_news'] = instance.date.strftime("%B %d, %Y")
+        ret['date_news'] = formats.date_format(instance.date)
         del ret['date']
         nb_product = instance.tags_video.count()
         if nb_product > 1:
-            ret['nb_product'] = "{} products".format(nb_product)
+            ret['nb_product'] = "{} produits".format(nb_product)
         else:
-            ret['nb_product'] = "{} product".format(nb_product)
+            ret['nb_product'] = "{} produit".format(nb_product)
         ret['timeline_value'] = (
             int((instance.expiration_date - timezone.now()) / (instance.expiration_date - instance.date) * 100)
         )
