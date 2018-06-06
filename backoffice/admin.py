ec4bc06424af164c1f4ac8f227af0056164243ff
npantel <nicolas.pantel@gmail.com>
2018-06-06 10:01:19 +0200

    feature(admin): Add MiwoUser filter Influencer/Follower

--- backoffice/admin.py
+++ backoffice/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from . import models
 
 
+class InfluencerFollowerListFilter(admin.SimpleListFilter):
+    """List filter by Influencer or Follower status"""
+    title = _("User status")
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        """Filter display"""
+        return (
+            ('INFLUENCERS', _('Influencers')),
+            ('FOLLOWERS', _("Followers")),
+        )
+
+    def queryset(self, request, queryset):
+        """Return Influencers or Followers MiwoUsers"""
+        if self.value() == 'INFLUENCERS':
+            return queryset.filter(campaigns__isnull=False).distinct()
+        if self.value() == 'FOLLOWERS':
+            return queryset.filter(campaigns__isnull=True)
+
+
 class MiwoUserAdmin(admin.ModelAdmin):
-    pass
+    list_filter = (InfluencerFollowerListFilter,)
 admin.site.register(models.MiwoUser, MiwoUserAdmin)
 
 
